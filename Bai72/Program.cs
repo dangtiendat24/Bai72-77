4dccc170a363b0bb2601fdbe7f80482dd08c1c53
dangtiendat24 <148854633+dangtiendat24@users.noreply.github.com>
2024-07-18 22:32:14 +0700

    Add files via upload

---
+++ Bai72/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Text;
+using Microsoft.Data.SqlClient;
+ namespace Database_Operation
+{
+    class DBConn
+    {
+        // Main Method
+        static void Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Connect();
+            Console.ReadKey();
+        }
+
+        static void Connect()
+        {
+            string constr;
+            SqlConnection conn;
+            constr = @"Data Source=DESKTOP-DI70DQQ\SQLEXPRESS;Initial Catalog=Demodb;User ID=LoginMoi;Password=Dangdat24@;TrustServerCertificate=True;";
+            conn = new SqlConnection(constr);
+            conn.Open();
+            Console.WriteLine("Mở kết nối!");
+            // Đóng kết nối
+            conn.Close();
+        }
+    }
+}
